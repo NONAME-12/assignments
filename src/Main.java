59889e419783ff8292c19179f253c1d842a83d7a
NONAME-12 <orazbay006@gmail.com>
2025-03-30 23:55:13 +0500

    Task 7

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,15 @@ public class Main {
     public static void main(String[] args) {
         int[] arr = {3,2,4,1,6};
         double startTime = System.nanoTime();
-        var result = power(2,7);
+        reverseArray();
         double endTime = System.nanoTime();
-        System.out.println(result);
         double duration = (endTime - startTime) / 1000000;
 
 
         System.out.println("Time taken:"+ duration+ " milliseconds");
 
     }
-    private static int power(int a,int n) {
-        return Solutions.power(a,n);
+    private static void reverseArray() {
+         Solutions.reverseArray(5);
     }
 }
