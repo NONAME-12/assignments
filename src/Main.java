ba7888f50122a8010b0ca52a2355c47ec0f7fb4c
NONAME-12 <orazbay006@gmail.com>
2025-03-30 23:45:48 +0500

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         int[] arr = {3,2,4,1,6};
         double startTime = System.nanoTime();
-        var result = getMin(arr,5);
+        var result = getAverage(arr,5);
         double endTime = System.nanoTime();
         System.out.println(result);
         double duration = (endTime - startTime) / 1000000;
@@ -12,7 +12,7 @@ public class Main {
         System.out.println("Time taken:"+ duration+ " milliseconds");
 
     }
-    private static int getMin(int[] arr,int n) {
-        return Solutions.getMin(arr,n);
+    private static double getAverage(int[] arr,int n) {
+        return Solutions.getAverage(arr,n);
     }
 }
